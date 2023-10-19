179e65eb0699ffcb435a94dcb79c3c0a7b869081
santanaIgot <santanaigoor01@gmail.com>
2023-10-19 20:39:51 -0300

    first commit

---
+++ inventario.py
@@ -0,0 +1,12 @@
+from flask import Flask, request, jsonify
+import cx_Oracle
+
+
+app = Flask(__name__) # essa linha é para importar o framework flask
+
+oracle_connection = cx_Oracle.connect()
+cursor = oracle_connection.cursor()
+
+# Rota para listar todos os intens do inventario 
+
+
