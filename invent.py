f0974b7c05728d6045fdbc55bc26ce44674727eb
santanaIgot <santanaigoor01@gmail.com>
2023-10-19 22:26:49 -0300

    Construindo API

--- invent.py
+++
@@ -1,8 +0,0 @@
-from flask import Flask, request, jsonify
-import cx_Oracle
-
-
-app = Flask(__name__)  # essa linha é para importar o framework flask
-
-oracle_connection = cx_Oracle.connect()
-cursor = oracle_connection.cursor()
